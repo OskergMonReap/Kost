6a960b7fbda6ad9056d4bea55ed4bceb93f18406
Travis <travis.howard.tj@gmail.com>
2019-01-21 17:57:48 -0500

    models buildout v1 (#9)
    * create README
    
    * read me expansion, basic. v2
    
    * Update README.md
    
    * simple test framework layout via tox
    
    * tox changes
    
    * Dockerfile, small Readme typo fixes
    
    * Began work on models.py (#7)
    
    * syntax changes (#8)
    
    * Began work on models.py
    
    * slight syntax change

--- food_journal/admin.py
+++ food_journal/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from food_journal.models import IngredientsMeta, MealMeta, MealInstance, IngredientInstance
 
 # Register your models here.
+admin.site.register(MealMeta)
+
+
+class IngredientsInline(admin.TabularInline):
+    model = IngredientInstance
+
+
+@admin.register(IngredientsMeta)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ['ingredient_name']
+    fields = ['ingredient_name']
+    inlines = [IngredientsInline]
+
+
+@admin.register(MealInstance)
+class MealsAdmin(admin.ModelAdmin):
+    list_display = ('meal_name', 'rating')
+    fields = ['meal_name', 'rating']
+    inlines = [IngredientsInline]
---
+++ food_journal/config/options.py
@@ -0,0 +1,26 @@
+RATINGS = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+)
+
+PREP_METHODS = (
+    ('baked', 'baked'),
+    ('boiled', 'boiled'),
+    ('braised', 'braised'),
+    ('broiled', 'broiled'),
+    ('fried', 'fried'),
+    ('grilled', 'grilled'),
+    ('pan-fried', 'pan-fried'),
+    ('raw', 'raw'),
+    ('simmered', 'simmered'),
+    ('smoked', 'smoked'),
+    ('steamed', 'steamed')
+)
