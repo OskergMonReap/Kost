a5f7f9884b7c779fbac2d9cf97adb18298d04e98
Travis <travis.howard.tj@gmail.com>
2019-02-13 10:48:54 -0500

    empty template creation, django version sec upgrade

--- food_journal/views.py
+++ food_journal/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import IngredientInstance, MealInstance, IngredientsMeta, MealMeta
 
 # Create your views here.
 
 
 def food_journal_view(request):
-    return HttpResponse("The view in food_journal is working!")
+    food_items = MealInstance.objects.all()
+    template = loader.get_template('home.html')
+    context = {
+        'food_items': food_items,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def food_listings(request):
+    food_items = MealInstance.objects.all()
+    return HttpResponse(content=food_items)
